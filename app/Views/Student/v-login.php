b3b43d4556c261fa04ff7770b64180e519eba97c
Sijull <fikaryunara@gmail.com>
2021-05-07 14:55:14 +0700

    ..

--- app/Views/Student/v-login.php
+++ app/Views/Student/v-login.php
@@ -1,17 +1,18 @@
 <!-----ini halaman login------>
-<div class="wrap-log">
-    <div class="container-fluid cont-logbox">
-        <form>
-            <label for="exampleInputEmail1">Login pelajar</label>
-            <div class="fg form-group">
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            </div>
-            <div class="fg form-group">
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-            </div>
-            <button type="submit" class="bot-log btn btn-primary">Masuk</button>
-            <button type="submit" class="bot-log btn btn-primary">Daftar</button>
-        </form>
-        <a class="bot-log-pen navbar-brand" href="#">Login pengajar</a>
-    </div><!-- /.container-fluid -->
-</div>
+<div class="content-wrapper">
+    <div class="wrap-log">
+        <div class="container-fluid cont-logbox">
+            <form>
+                <label for="exampleInputEmail1">Login pelajar</label>
+                <div class="fg form-group">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="fg form-group">
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <button type="submit" class="bot-log btn btn-primary">Masuk</button>
+                <button type="submit" class="bot-log btn btn-primary">Daftar</button>
+            </form>
+            <a class="bot-log-pen navbar-brand" href="#">Login pengajar</a>
+        </div><!-- /.container-fluid -->
+    </div>
--- app/Views/studentreg/v-register.php
+++ app/Views/studentreg/v-register.php
@@ -1,21 +1,22 @@
 <!-----ini halaman login------>
-<div class="wrap-log">
-    <div class="container-fluid cont-logbox">
-        <form>
-            <label for="exampleInputEmail1">Daftar pelajar</label>
-            <div class="fg form-group">
-                <input type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Full name">
-            </div>
-            <div class="fg form-group">
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            </div>
-            <div class="fg form-group">
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-            </div>
-            <div class="fg form-group">
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password Confirmation">
-            </div>
-            <button type="submit" class="bot-log btn btn-primary">Daftar</button>
-        </form>
-    </div><!-- /.container-fluid -->
-</div>
+<div class="content-wrapper">
+    <div class="wrap-log">
+        <div class="container-fluid cont-logbox">
+            <form>
+                <label for="exampleInputEmail1">Daftar pelajar</label>
+                <div class="fg form-group">
+                    <input type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Full name">
+                </div>
+                <div class="fg form-group">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="fg form-group">
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="fg form-group">
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password Confirmation">
+                </div>
+                <button type="submit" class="bot-log btn btn-primary">Daftar</button>
+            </form>
+        </div><!-- /.container-fluid -->
+    </div>
