2bcd832262f24284b6394ed0b19e02b2300dfac3
jarfajar2314 <uchihafajar2314@gmail.com>
2021-05-12 21:23:34 +0700

    benerin dashboard pengajar

--- app/Views/Teacher/v-dashboard.php
+++ app/Views/Teacher/v-dashboard.php
@@ -9,66 +9,60 @@
             </div>
             <!-- /.col -->
             <!-- Profile -->
-            <div class="col mb-5">
+            <div class="col mb-lg-5 mb-3">
                 <h1 class="fw-bold text-center text-lg-start">Halo, Margareth!</h1>
                 <br>
-                <div class="d-flex ms-lg-0 ms-5">
-                    <div class="d-flex">
-                        <div class="me-3">
-                            <p><i class="fas fa-user"></i></p>
-                        </div>
-                        <div class="me-3">
-                            <p>Margareth Fargo</p>
-                        </div>
+                <div class="row ms-4 ms-lg-0">
+                    <div class="col-1">
+                        <p><i class="fas fa-user"></i></p>
                     </div>
-                    <div class="d-flex">
-                        <div class="me-3">
-                            <p><i class="fas fa-map-marker-alt"></i></p>
-                        </div>
-                        <div class="me-3">
-                            <p>Bandung</p>
-                        </div>
+                    <div class="col">
+                        <p>Margareth Fargo</p>
                     </div>
                 </div>
-                <div class="d-flex ms-lg-0 ms-5">
-                    <div class="d-flex">
-                        <div class="me-3">
-                            <p><i class="fas fa-envelope"></i></p>
-                        </div>
-                        <div class="me-3">
-                            <p>margarethfargo@gmail.com</p>
-                        </div>
+                <div class="row ms-4 ms-lg-0">
+                    <div class="col-1">
+                        <p><i class="fas fa-envelope"></i></p>
                     </div>
-                    <div class="d-flex">
-                        <div class="me-3">
-                            <p><i class="fas fa-dollar-sign"></i></p>
-                        </div>
-                        <div class="me-3">
-                            <p>Rp 100.000,-</p>
-                        </div>
+                    <div class="col">
+                        <p>margarethfargo@gmail.com</p>
                     </div>
                 </div>
-                <div class="d-flex ms-lg-0 ms-5">
-                    <div class="d-flex">
-                        <div class="me-3">
-                            <p><i class="fas fa-book"></i></p>
-                        </div>
-                        <div class="me-3">
-                            <p>Matematika</p>
-                        </div>
+                <div class="row ms-4 ms-lg-0">
+                    <div class="col-1">
+                        <p><i class="fas fa-map-marker-alt"></i></p>
                     </div>
-                    <div class="d-flex">
-                        <div class="me-3">
-                            <p><i class="fas fa-graduation-cap"></i></p>
-                        </div>
-                        <div class="me-3">
-                            <p>Perguruan Tinggi</p>
-                        </div>
+                    <div class="col">
+                        <p>Bandung</p>
+                    </div>
+                </div>
+                <div class="row ms-4 ms-lg-0">
+                    <div class="col-1">
+                        <p><i class="fas fa-dollar-sign"></i></p>
+                    </div>
+                    <div class="col">
+                        <p>Rp 100.000,-</p>
+                    </div>
+                </div>
+                <div class="row ms-4 ms-lg-0">
+                    <div class="col-1">
+                        <p><i class="fas fa-book"></i></p>
+                    </div>
+                    <div class="col">
+                        <p>Matematika</p>
+                    </div>
+                </div>
+                <div class="row ms-4 ms-lg-0">
+                    <div class="col-1">
+                        <p><i class="fas fa-graduation-cap"></i></p>
+                    </div>
+                    <div class="col">
+                        <p>Perguruan Tinggi</p>
                     </div>
                 </div>
             </div>
             <!-- /.col -->
-            <div class="col-lg-3 pt-lg-5    ">
+            <div class="col-lg-3 pt-lg-5 text-center text-lg-start">
                 <div class="">
                     <a class="btn btn-green btn-sm" href="#" role="button"><i class="fas fa-edit"></i> Ubah Profil</a>
                 </div>
@@ -123,27 +117,3 @@
     </div>
     <!-- wrapper -->
 </div>
-
-<script>
-  // Untuk Tabs di Dashboard
-  $(document).ready(function(){
-    $("#nav-riwayat-tab").click(function(){
-      $("#nav-riwayat-tab").attr("class", "nav-item nav-link link-green active");
-      $("#nav-jadwal-tab").attr("class", "nav-item nav-link link-green");
-      $("#nav-riwayat").attr("class", "tab-pane fade show active");
-      $("#nav-jadwal").attr("class", "tab-pane fade");
-      $("#nav-riwayat").removeClass("fadeIn");
-      $("#nav-riwayat").addClass("fadeIn");
-      return false;
-    });
-    $("#nav-jadwal-tab").click(function(){
-      $("#nav-jadwal-tab").attr("class", "nav-item nav-link link-green active");
-      $("#nav-riwayat-tab").attr("class", "nav-item nav-link link-green");
-      $("#nav-jadwal").attr("class", "tab-pane fade show active");
-      $("#nav-riwayat").attr("class", "tab-pane fade");
-      $("#nav-jadwal").removeClass("fadeIn");
-      $("#nav-jadwal").addClass("fadeIn");
-      return false;
-    });
-  });
-</script>
