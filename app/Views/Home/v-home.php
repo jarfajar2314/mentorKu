6489e251cb5c9fb266c884f4375276d6e89914e1
Sijull <fikaryunara@gmail.com>
2021-05-14 20:10:35 +0700

    add home (not finished yet)

---
+++ app/Views/Home/v-home.php
@@ -0,0 +1,18 @@
+<header class="container-fluid header-cont">
+    <div class="container-fluid cont-header">
+        <h1 class="header-text text-center">Ingin belajar apa hari ini?</h1>
+        <div class="cont-searchbox">
+            <div class="row">
+                <div class="col-10">
+                    <input type="text" class="form-control" placeholder="Pilih subjek" aria-label="pilih subjek">
+                </div>
+                <div class="col-2">
+                    <button type="submit" class="bot-log btn btn-primary">cari</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container-fluid home-cont"></div>
+    
+</div>
