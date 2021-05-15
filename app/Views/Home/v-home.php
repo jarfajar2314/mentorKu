9b7f594b93f2638600f133ebfccd2b86cb184370
Sijull <fikaryunara@gmail.com>
2021-05-15 21:13:41 +0700

    homepage done

--- app/Views/Home/v-home.php
+++ app/Views/Home/v-home.php
@@ -17,46 +17,97 @@
     <h4 class="homecontext">Subjek Terpopuler</h4>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3">
         <div class="col">
-            <div class="cont-homecontent">
-                
+            <div class="card shadow-sm">
+            <img src="<?= base_url() ?>/imageSource/chris-liverani-rD2dc_2S3i0-unsplash (1).jpg" class="bd-placeholder-img card-img-top" width="100%" height="225">
+            <div class="card-body">
+              <a href="#" class="card-text subjecttext">Matematika</a>
             </div>
+          </div>
         </div>
         <div class="col">
-            <div class="cont-homecontent">
-                
+            <div class="card shadow-sm">
+            <img src="<?= base_url() ?>/imageSource/engin-akyurt-KUeJcc4YUug-unsplash.jpg" class="bd-placeholder-img card-img-top" width="100%" height="225">
+            <div class="card-body">
+              <a href="#" class="card-text subjecttext">Fisika</a>
             </div>
+          </div>
         </div>
         <div class="col">
-            <div class="cont-homecontent">
-                
+            <div class="card shadow-sm">
+            <img src="<?= base_url() ?>/imageSource/terry-vlisidis-RflgrtzU3Cw-unsplash (1).jpg" class="bd-placeholder-img card-img-top" width="100%" height="225">
+            <div class="card-body">
+              <a href="#" class="card-text subjecttext">Kimia</a>
             </div>
+          </div>
         </div>
         <div class="col">
-            <div class="cont-homecontent">
-                
+            <div class="card shadow-sm">
+            <img src="<?= base_url() ?>/imageSource/artiom-vallat-r8_Pzl9XuPc-unsplash (1).jpg" class="bd-placeholder-img card-img-top" width="100%" height="225">
+            <div class="card-body">
+              <a href="#" class="card-text subjecttext">Biologi</a>
             </div>
+          </div>
         </div>
     </div>
     <h4 class="homecontext">Mentor Favorit</h4>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3">
         <div class="col">
-            <div class="cont-homecontent">
-                
+            <div class="cont-homecontent2">
+            <img src="<?= base_url() ?>/imageSource/default-mentor.png" class="bd-placeholder-img rounded-circle" width="160" height="160" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <a href="#" class="card-text subjecttext">
+                    Nama mentor
+                </a>
+                <p href="#" class="card-text subjecttext">
+                    Subject
+                </p>
             </div>
         </div>
         <div class="col">
-            <div class="cont-homecontent">
-                
+            <div class="cont-homecontent2">
+            <img src="<?= base_url() ?>/imageSource/default-mentor.png" class="bd-placeholder-img rounded-circle" width="160" height="160" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <a href="#" class="card-text subjecttext">
+                    Nama mentor
+                </a>
+                <p href="#" class="card-text subjecttext">
+                    Subject
+                </p>
             </div>
         </div>
         <div class="col">
-            <div class="cont-homecontent">
-                
+            <div class="cont-homecontent2">
+            <img src="<?= base_url() ?>/imageSource/default-mentor.png" class="bd-placeholder-img rounded-circle" width="160" height="160" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <a href="#" class="card-text subjecttext">
+                    Nama mentor
+                </a>
+                <p href="#" class="card-text subjecttext">
+                    Subject
+                </p>
             </div>
         </div>
         <div class="col">
-            <div class="cont-homecontent">
-                
+            <div class="cont-homecontent2">
+            <img src="<?= base_url() ?>/imageSource/default-mentor.png" class="bd-placeholder-img rounded-circle" width="160" height="160" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <a href="#" class="card-text subjecttext">
+                    Nama mentor
+                </a>
+                <p href="#" class="card-text subjecttext">
+                    Subject
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid about-cont">
+    <div class="row featurette">
+        <div class="col-md-6">
+            <img src="<?= base_url() ?>/imageSource/about-image.png" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500">
+        </div>
+        <div class="col-md-6">
+            <div class="about-text-box">
+                <h2 class="featurette-heading about-head">Apa itu Mentorku.io</h2>
+                <p class="lead about-text">Mentorku.io adalah platform bimbingan belajar privat berbasis web application yang dapat membantu kamu mencari mentor untuk belajar. 
+                    Mentorku.io bekerja sama dengan mentor-mentor terbaik dari berbagai bidang studi yang siap membimbing kamu dalam belajar ataupun membantu kamu 
+                    mempersiapkan diri menghadapi ujian.</p>
             </div>
         </div>
     </div>
