98ef0f5da5736d4b23df5f79f994592a40f42506
Sijull <fikaryunara@gmail.com>
2021-05-15 11:32:03 +0700

    add more to home

--- app/Views/Home/v-home.php
+++ app/Views/Home/v-home.php
@@ -1,28 +1,64 @@
-<div class="container-fluid">
-    <header class="container-fluid header-cont">
-        <div class="container-fluid cont-header">
-            <h1 class="header-text text-center">Ingin belajar apa hari ini?</h1>
-            <div class="cont-searchbox">
-                <div class="row">
-                    <div class="col-10">
-                        <input type="text" class="form-control" placeholder="Pilih subjek" aria-label="pilih subjek">
-                    </div>
-                    <div class="col-2">
-                        <button type="submit" class="bot-log btn btn-primary">cari</button>
-                    </div>
+<header class="container-fluid header-cont">
+    <div class="container-fluid cont-header">
+        <h1 class="header-text text-center">Ingin belajar apa hari ini?</h1>
+        <div class="cont-searchbox">
+            <div class="row">
+                <div class="col-10">
+                    <input type="text" class="form-control" placeholder="Pilih subjek" aria-label="pilih subjek">
+                </div>
+                <div class="col-2">
+                    <button type="submit" class="bot-log btn btn-primary">cari</button>
                 </div>
             </div>
         </div>
-    </header>
-    <div class="container-fluid home-cont"></div>
-        <div class="row">
-          <div class="col-md-7 order-md-2">
-            
-          </div>
-          <div class="col-md-5 order-md-1">
-            
-    
-          </div>
+    </div>
+</header>
+<div class="container-fluid home-cont">
+    <h4 class="homecontext">Subjek Terpopuler</h4>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3">
+        <div class="col">
+            <div class="cont-homecontent">
+                
+            </div>
+        </div>
+        <div class="col">
+            <div class="cont-homecontent">
+                
+            </div>
+        </div>
+        <div class="col">
+            <div class="cont-homecontent">
+                
+            </div>
+        </div>
+        <div class="col">
+            <div class="cont-homecontent">
+                
+            </div>
+        </div>
+    </div>
+    <h4 class="homecontext">Mentor Favorit</h4>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3">
+        <div class="col">
+            <div class="cont-homecontent">
+                
+            </div>
+        </div>
+        <div class="col">
+            <div class="cont-homecontent">
+                
+            </div>
+        </div>
+        <div class="col">
+            <div class="cont-homecontent">
+                
+            </div>
+        </div>
+        <div class="col">
+            <div class="cont-homecontent">
+                
+            </div>
         </div>
     </div>
 </div>
+    
