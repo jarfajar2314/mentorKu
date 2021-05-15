27064fb4ce382ba6bebc491553bbfaa0a04a51ab
Sijull <fikaryunara@gmail.com>
2021-05-15 08:25:31 +0700

    Update v-home.php

--- app/Views/Home/v-home.php
+++ app/Views/Home/v-home.php
@@ -1,18 +1,28 @@
-<header class="container-fluid header-cont">
-    <div class="container-fluid cont-header">
-        <h1 class="header-text text-center">Ingin belajar apa hari ini?</h1>
-        <div class="cont-searchbox">
-            <div class="row">
-                <div class="col-10">
-                    <input type="text" class="form-control" placeholder="Pilih subjek" aria-label="pilih subjek">
-                </div>
-                <div class="col-2">
-                    <button type="submit" class="bot-log btn btn-primary">cari</button>
+<div class="container-fluid">
+    <header class="container-fluid header-cont">
+        <div class="container-fluid cont-header">
+            <h1 class="header-text text-center">Ingin belajar apa hari ini?</h1>
+            <div class="cont-searchbox">
+                <div class="row">
+                    <div class="col-10">
+                        <input type="text" class="form-control" placeholder="Pilih subjek" aria-label="pilih subjek">
+                    </div>
+                    <div class="col-2">
+                        <button type="submit" class="bot-log btn btn-primary">cari</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</header>
-<div class="container-fluid home-cont"></div>
+    </header>
+    <div class="container-fluid home-cont"></div>
+        <div class="row">
+          <div class="col-md-7 order-md-2">
+            
+          </div>
+          <div class="col-md-5 order-md-1">
+            
     
+          </div>
+        </div>
+    </div>
 </div>
